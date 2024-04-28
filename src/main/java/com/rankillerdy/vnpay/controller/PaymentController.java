a047e2809b1a010c296ee288d9c4925ea3eef7d6
Đinh Gia Bảo <107354883+RankillerDY@users.noreply.github.com>
2024-04-28 22:31:40 +0700

    implement config and controller

---
+++ src/main/java/com/rankillerdy/vnpay/controller/PaymentController.java
@@ -0,0 +1,20 @@
+package com.rankillerdy.vnpay.controller;
+
+import com.rankillerdy.vnpay.service.PaymentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/payment")
+public class PaymentController {
+    public final PaymentService paymentService;
+
+    @GetMapping("/create-payment")
+    public ResponseEntity<?> createPayment() {
+        paymentService.createPayment();
+    }
+}
